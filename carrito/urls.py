32391ea60f3038fe9c9d6f1bb07abc52fc66ccc6
Fabio <dahofcmmejia@gmail.com>
2023-05-10 12:50:47 -0400

    Carrito, perfil se agregaron funciones

--- carrito/urls.py
+++ carrito/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', carrito, name='carrito'),
+    path('agregar/<int:producto_id>/', agregar_carrito, name='addCarrito'),
+    path('eliminar/<int:producto_id>/', eliminar_carrito, name='removeCarrito'),
+    path('reducir/<int:producto_id>/', reducir_cantidad, name='reducirCarrito'),
+    path('limpiar/', limpiar_carrito, name='limpiarCarrito'),
 ]
--- carrito/views.py
+++ carrito/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .cart import Carrito
+from producto.models import Productos
 
 # Create your views here.
 def carrito(request):
@@ -6,3 +8,25 @@ def carrito(request):
     return render(request, 'carrito.html', context)
 
 
+def agregar_carrito(request, product_id):
+    carrito = Carrito(request)
+    producto = Productos.objects.get(id=product_id)
+    carrito.add(producto)
+    return redirect('productos')
+
+def eliminar_carrito(request, product_id):
+    carrito = Carrito(request)
+    producto = Productos.objects.get(id=product_id)
+    carrito.remove(producto)
+    return redirect('productos')
+
+def reducir_cantidad(request, product_id):
+    carrito = Carrito(request)
+    producto = Productos.objects.get(id=product_id)
+    carrito.decrementar(producto)
+    return redirect('productos')
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.clear()
+    return redirect('productos')
